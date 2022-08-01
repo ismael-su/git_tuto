146208b2e8fa88c0fa50710b2798049c01a8c6a8
unknown <ahmetrabe2000@gmail.com>
2022-08-02 00:42:39 +0100

    Fix ignored files step 1

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse('<h2>Hello Django</h2>')
+    return HttpResponse('<h2>Hello Django I new on django</h2>')
 
 
