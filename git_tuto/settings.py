2c83eea47e93cad842589a12a4a397c51d2e8ce0
Ahamet2000 <72851505+Ahamet2000@users.noreply.github.com>
2022-08-02 01:15:02 +0100

    Rabe (#2)
    * I have add the new app listings for MVT architure
    
    * Fix ignored files step 1
    
    * removed gitignore
    
    * Fixed ignored files

--- git_tuto/settings.py
+++ git_tuto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'listings' #i add here the app listings for MVT architecure
 ]
 
 MIDDLEWARE = [
