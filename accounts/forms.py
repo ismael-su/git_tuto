2f1ea85a537ccbdb18d539690100b15278dc7823
unknown <ahmetrabe2000@gmail.com>
2022-08-04 00:36:04 +0100

    I also deleted the accounts app to create my own struture

--- accounts/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class UserLoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Nom d'utilisateur"}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Mots de passe"}))
-
